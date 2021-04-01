ffb2b9321bf9619c3f5ecd42c5ba4bc5dcef16f8
Martin Rostan <mrostan@infragistics.com>
2021-04-01 14:42:37 -0300

    samples updated to latest API
    (cherry picked from commit 629e147911e8a24e57e890631ff3658daaf7c615)
    
    # Conflicts:
    #	upmedia-backend-tomcat/src/main/java/com/pany/analytics/upmedia/reveal/WebAppListener.java

--- upmedia-backend-tomcat/src/main/java/com/pany/analytics/upmedia/reveal/WebAppListener.java
+++ upmedia-backend-tomcat/src/main/java/com/pany/analytics/upmedia/reveal/WebAppListener.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+import com.infragistics.reveal.engine.init.InitializeParameterBuilder;
 import com.infragistics.reveal.engine.init.RevealEngineInitializer;
 
 @WebListener
@@ -17,8 +18,13 @@ public class WebAppListener implements ServletContextListener {
 
 	@Override
 	public void contextInitialized(ServletContextEvent ctx) {
-		RevealEngineInitializer.initialize(new UpmediaAuthenticationProvider(), new UpmediaUserContextProvider(), new UpmediaDashboardProvider(), null, null);
-		
+		RevealEngineInitializer.initialize(new InitializeParameterBuilder().
+				setAuthProvider(new UpmediaAuthenticationProvider()).
+				setUserContextProvider(new UpmediaUserContextProvider()).
+				setDashboardProvider(new UpmediaDashboardProvider()).
+			build()
+		);
+
 		RevealEngineInitializer.registerResource(CorsFilter.class);
 	}
 
