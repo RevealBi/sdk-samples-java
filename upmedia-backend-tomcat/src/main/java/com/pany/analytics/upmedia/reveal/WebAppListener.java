3d52964d29ca11d80c788d7ba749359729170189
MLopezIG <52332681+MLopezIG@users.noreply.github.com>
2021-06-11 17:07:02 -0300

    Avoid CORS failed requests when reveal-sdk responds with status 500 (#8)

--- upmedia-backend-tomcat/src/main/java/com/pany/analytics/upmedia/reveal/WebAppListener.java
+++ upmedia-backend-tomcat/src/main/java/com/pany/analytics/upmedia/reveal/WebAppListener.java
@@ -39,6 +39,8 @@ public class WebAppListener implements ServletContextListener {
 			build()
 		);
 
+		RevealEngineInitializer.registerResource(ExceptionMapper.class);
+
 		//register the CORS filter class
 		RevealEngineInitializer.registerResource(CorsFilter.class);
 		
